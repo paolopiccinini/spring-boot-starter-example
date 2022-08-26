025c57680c5f5e27c26b73362096374362c5acf2
paolopiccinini <bastone.88@gmail.com>
2022-08-26 10:42:14 +0200

    Remove @Configuration, and Added @AutoCOnfiguration of Config Class

--- spring-boot-starter-example/src/main/java/com/example/springbootstarterexample/configuration/Config.java
+++ spring-boot-starter-example/src/main/java/com/example/springbootstarterexample/configuration/Config.java
@@ -1,6 +1,7 @@
 package com.example.springbootstarterexample.configuration;
 
 import org.hibernate.annotations.Immutable;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +12,8 @@ import com.example.springbootstarterexample.controller.SomeEntityController;
 import com.example.springbootstarterexample.domain.SomeEntity;
 import com.example.springbootstarterexample.service.SomeServiceImpl;
 
-@Configuration
-@AutoConfigureAfter(JpaRepositoriesAutoConfiguration.class)
+@AutoConfiguration(after = JpaRepositoriesAutoConfiguration.class)
+//@AutoConfigureAfter(JpaRepositoriesAutoConfiguration.class)
 @EnableJpaRepositories(basePackages = "com.example.springbootstarterexample.repository")
 @Import({SomeServiceImpl.class, SomeEntityController.class})
 public class Config {
